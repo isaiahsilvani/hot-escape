50c9c9325b2a32cbdc4d8a1fe7d9f17f9fd65673
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-04-13 15:41:22 -0400

    add flight model

---
+++ models/flight.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    originCity: {type:String},
+    originStation:{type: String},
+    destinationCity: {type:String},
+    airline:{type:String},
+    direct:{type: Boolean},
+    stops: {type: Boolean},
+    lowestPrice : {type: Number},
+    currency: {type: String},
+    }, {
+      timestamps: true
+    });
+
+
+module.exports = mongoose.model('Flight', flightSchema);
