fceb6101e4a1caa3633538d7bfee40a5c82d4f60
snyles <sniles@gmail.com>
2021-04-13 17:07:06 -0400

    display place search results

--- src/components/SearchPlace/SearchPlace.jsx
+++ src/components/SearchPlace/SearchPlace.jsx
@@ -3,14 +3,17 @@ import * as flightAPI from '../../services/flightService'
 
 class SearchPlace extends Component {
   state = { 
+    places: [],
     formData: {
       query: '',
     }
   }
 
-  sendRequest = () => {
-    const query = "Miami"
-    flightAPI.searchPlace(query);
+  sendRequest = async () => {
+    const query = "Miami";
+    const results = await flightAPI.searchPlace(query);
+    console.log(query, results.Places);
+    this.setState({places: results.Places})
   }
 
   handleChange = e => {
@@ -24,6 +27,11 @@ class SearchPlace extends Component {
   render() { 
     return ( 
       <>
+        <div>{this.state.places.map((place, idx) =>
+          <p key={idx}>
+            {place.PlaceName}
+          </p>)}
+        </div>
         <button onClick={this.sendRequest}>Send</button>
       </>
      );
