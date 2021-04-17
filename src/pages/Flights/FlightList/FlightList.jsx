65df0734378b69b94286a9c53edccfc06412dc3a
snyles <sniles@gmail.com>
2021-04-17 19:20:58 -0400

    add basic flight section to itinerary view

---
+++ src/pages/Flights/FlightList/FlightList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FlightList (props) {
+
+
+  return (
+    <h1>Flight List</h1>
+  );
+}
---
+++ src/pages/Flights/FlightSection/FlightSection.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import FlightSearch from '../FlightSearch/FlightSearch'
+import FlightList from '../FlightList/FlightList'
+
+import './FlightSection.css'
+
+export default function FlightSection(props) {
+  const [display, setDisplay] = useState('view');
+  const [flightId, setFlightId] = useState(123);
+
+  const displaySwitch = () => {
+    switch(display) {
+      case 'search':
+        return <FlightSearch />
+      case 'view':
+        return <h1>Flight #{flightId}</h1>
+      case 'list':
+      default:
+        return <FlightList />
+    }
+  }
+
+  return (
+    <main>
+      <h1>Flights</h1>
+      <div className='section-nav'>
+        <div onClick={()=>setDisplay('list')}>
+          My Flights</div>
+        <div onClick={()=>setDisplay('search')}>
+          Search Flights</div>
+        <div onClick={()=> {
+          setDisplay('view');
+          setFlightId(666)}}>
+          Flight Details</div>
+      </div>
+      {displaySwitch(flightId)}
+    </main>
+  )
+}
