a19cc7d4dd7d70b1b10f9b00abd218ffcb382883
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-04-18 15:10:37 -0400

    add attraction display function

--- src/pages/Itinerary/ItineraryView/ItineraryView.jsx
+++ src/pages/Itinerary/ItineraryView/ItineraryView.jsx
@@ -47,8 +47,7 @@ export default function ItineraryView({user}) {
         return <FlightSection setFlight={setFlight} flights={itineraryData.flights} />
     }
   }
-    
-    
+      
   return (
     <>
     <main>
