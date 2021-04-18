178bb224b97d2c553b353fc946ea2e00e705c3c3
janae-b <janae.boersma@comcast.net>
2021-04-18 10:57:53 -0500

    fix-hotels

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -10,16 +10,16 @@ module.exports = {
 }
 
 function create(req, res) {
-  Itinerary.find({_id:req.body.itinID, owner:req.user._id})
+  Itinerary.findById(req.body.itinID)
   .then(itinerary => {
     console.log(itinerary)
     console.log(req.body)
     itinerary.hotels.push(req.body)
     itinerary.save()
-    console.log(itinerary)
-    .then(itinerary => res.json(itinerary))
+    .then(() => {
+      res.json(itinerary)
+    })
   })
-  .catch(err => {res.json(err)})
 }
 
 function index(req,res) {
