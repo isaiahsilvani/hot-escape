c3fbd471ec7ef7b28c1d95b2d742817b0397a1d0
janae-b <janae.boersma@comcast.net>
2021-04-19 09:14:11 -0500

    update hotel layout

--- src/services/hotelService.js
+++ src/services/hotelService.js
@@ -11,3 +11,9 @@ export function addHotel(hotelData) {
     },{mode: "cors" })
     .then(res => res.json())
   }
+
+// export function deleteOne(id) {
+//     return fetch(`${BASE_URL}/${id}`, {
+//       method: 'DELETE'
+//     }).then(res => res.json());
+//   }
