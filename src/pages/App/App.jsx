d082bfc76c7b96bd03b5f5df0d7fa21dcee07255
janae-b <janae.boersma@comcast.net>
2021-04-21 17:29:19 -0500

    added more to app description

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -81,10 +81,10 @@ export default function App (props) {
               
               <main>
                 <div className='App'>
-                <h2>Welcome to Hot Escape</h2>
-                <h3>Your next vacation is waiting for you, all you've got to do is plan it.</h3>
-                <p> Use Hot Escape to plan your next vacation. You can: <br/> <br/>
-                  1. Create an itinerary for each of your vacation plans <br/>2. Search for flights and add them to your itinerary<br/>3. Add the hotels that you're planning to stay at <br/>4. Chat with others about their vacations </p> 
+                <h1>Welcome to Hot Escape</h1>
+                <h2>Your next vacation is waiting for you, all you've got to do is plan it.</h2>
+                 <h3>Use Hot Escape to plan your next vacation. You can: </h3> 
+                 <p>  1. Create an itinerary for each of your vacation plans <br/>2. Search for flights and add them to your itinerary<br/>3. Add the hotels that you're planning to stay at <br/>4. Add the attractions you plan to see <br/>5. Add in the rentals you plan to use for transportation <br/>6. Chat with others about their vacations </p> 
                 <h3>Safe Travels!</h3>
                 </div>
               </main>
