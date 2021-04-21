7b8d4d710b9d87c505b1aa682594f44bd120de63
isaiahsilvani <isaiahsilvani@outlook.com>
2021-04-20 20:25:23 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 const httpServer = http.createServer(app)
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://hot-escapes.herokuapp.com/",
     methods: ["GET", "POST"]
   }
 });
