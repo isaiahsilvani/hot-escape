894c668b077e0e713d133eb8b3ddc50a07210ae1
snyles <sniles@gmail.com>
2021-04-21 14:52:22 -0400

    change itinerary list view, itinerary details card

--- src/components/ItineraryDetails/ItineraryDetails.jsx
+++ src/components/ItineraryDetails/ItineraryDetails.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import styles from './ItineraryDetails.module.css'
 
-export default function ItineraryDetails({itinData}) {
+export default function ItineraryDetails({itinData, center}) {
 
   const departure = new Date(itinData.startDate).toLocaleDateString();
   const returnDate = new Date(itinData.endDate).toLocaleDateString();
 
   return (
     // <main>
-      <div className={`whiteBox ${styles.twoColumns}`}>
-        <div className={styles.colOne}>
+      <div className={`${styles.box} ${center ? 'm-auto': ''}`}>
+        <div className={styles.itinDetails}>
           <img src={itinData.imageSrc} />
-        </div>
-        <div className={styles.colTwo}>
-            <h2>Escape from {itinData.origin} to {itinData.destination}</h2>
+          <div className={styles.details}>
+            <h1>Escape from {itinData.origin} to {itinData.destination}</h1>
             <p>
-              Starts on <strong>{departure}</strong>
+              Starts <strong>{departure}</strong><br />
+              Ends <strong>{returnDate}</strong>
             </p>
-            <p>
-              Ends on <strong>{returnDate}</strong>
-            </p>
-        
+          </div>
         </div>
       </div>
     // </main>
