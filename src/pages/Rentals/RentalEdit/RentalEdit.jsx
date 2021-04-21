f619b5dcf2bce10450324b07e3ab66ae21de723a
Alejandra Lopez <alejandracamperovega@gmail.com>
2021-04-21 15:25:51 -0400

    add edit rental page

---
+++ src/pages/Rentals/RentalEdit/RentalEdit.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useForm } from '../../../hooks/useForm'
+import * as rentalAPI from '../../../services/rentalService'
+
+export default function rentalEdit(props) {
+  const rental = props.itinData.rentals[props.rentalId]
+  const [invalidForm, setValidForm] = useState(true);
+  const [state, handleChange] = useForm({
+      company: rental?.company,
+      pickupTime: rental?.pickupTime,
+      dropoffTime: rental?.dropoffTime,
+      pickupDate: rental?.pickupDate,
+      dropoffDate: rental?.dropoffDate,
+  })
+    
+  const formRef = useRef();
+
+  useEffect(() => {
+      formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
+  });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+          const rentalData = {rental: state, itinID: props.itinID, rentalId: rental._id}
+          const newItin = await rentalAPI.updaterental(rentalData)
+          console.log("newItin", newItin)
+          props.setItineraryData(newItin)
+          props.setDisplay('list')
+        } catch (err) {
+          console.log(err.message)
+        }
+    }
+
+    return (
+        <>
+          <h1>Edit Rental</h1>
+          <div className="userForm">
+          <form autoComplete="off" ref={formRef} onSubmit={handleSubmit}>
+            <label>Company Name (required)
+            <input
+              className="form-control"
+              name="company"
+              value={state.company}
+              onChange={handleChange}
+              required
+            /></label>
+            <label>Pick-up Time (required)
+            <input
+              name="pickupTime"
+              value={state.pickupTime}
+              onChange={handleChange}
+              required
+            /></label>
+            <label>Drop-off Time (required)
+            <input
+              name="dropoffTime"
+              value={state.dropoffTime}
+              onChange={handleChange}
+              required
+            /></label>
+            <label>Pick-up Date
+            <input
+              type="date"
+              name="pickupDate"
+              value={state.pickupDate}
+              onChange={handleChange}
+            /></label>
+            <label>Drop-off Date
+            <input
+              type="date"
+              name="dropoffDate"
+              value={state.dropoffDate}
+              onChange={handleChange}
+            /></label>
+            <button
+              type="submit"
+              disabled={invalidForm}
+            >
+              UPDATE RENTAL
+            </button>
+          </form>
+        </div>
+        </>
+    )
+}
