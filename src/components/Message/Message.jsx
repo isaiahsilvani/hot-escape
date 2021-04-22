d29e606879231645ecc29fbaa13a01bb742ad038
isaiahsilvani <isaiahsilvani@outlook.com>
2021-04-22 14:50:56 -0400

    set message input to bottom of container

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -16,15 +16,16 @@ const Message = ({ message: { text, user }, name }) => {
     isSentByCurrentUser
       ? (
         <span className="messageContainer flex-end">
-          <p className="sentText pr-10">{compareName}</p>
-          <span className="messageBox backgroundBlue">
-            <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+            <span class='text'>
+              <span className="sentText pr-10">{compareName}</span>
+              <span className="messageBox backgroundBlue">
+              <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+            </span>
           </span>
         </span>
         )
         : (
           <span className="messageContainer flex-start">
-            <p className="sentText pl-10 ">{user}</p>
             <span className="messageBox backgroundLight">
               <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
             </span>
--- src/pages/Chatroom/Chatroom.jsx
+++ src/pages/Chatroom/Chatroom.jsx
@@ -123,7 +123,7 @@ const Chat = ( props ) => {
 
       
         
-    })
+    }, [roomData])
       
 
       return (
