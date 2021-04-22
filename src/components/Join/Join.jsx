f2dd62ec8cf6dd1b56142b3bfa74fbbf30cd9c7b
isaiahsilvani <isaiahsilvani@outlook.com>
2021-04-22 09:59:13 -0400

    fix ocreate room error

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -9,22 +9,12 @@ const Join = () => {
     const [room, setRoom] = useState('')
     const [rooms, setRooms] = useState([])
 
-    const handleCreateRoom = async () => {
-        // If the Room does not already exist in database, create it. If it already exists, do nothing
-        console.log('handle create room hit')
-        const data = {name, room}
-          const results = await chatAPI.createRoom(data); 
-          // console.log(query, results.Places);
-          
-      }
-
       useEffect(() => {
         // recieve room data here
         // if room didn't load on first useEffect, try again!
         
           async function fetchData(room) {
             // You can await here
-            console.log('pass room ', room, 'as argument for fetchRoomData')
             const response = await chatAPI.getRooms(room);
             setRooms(response)
             // ...
@@ -43,7 +33,7 @@ const Join = () => {
                     onClick={e => (!name || !room) ? e.preventDefault() : null}
                     to={`/chatroom?name=${name}&room=${room}`}
                     query={`${name}/${room}`}>
-                        <button className="button" type="submit" onClick={handleCreateRoom}>Enter Room</button>
+                        <button className="button" type="submit">Enter Room</button>
                     </Link>
                 </div>
             </div>
@@ -51,7 +41,6 @@ const Join = () => {
                 {/*///git push <remotename> <commit SHA>:<remotebranchname></remotebranchname>*/}
             {rooms.map((roomItem, idx) => 
             <div key={idx}>
-                {console.log(roomItem)}
                 <Link 
                     onClick={e => (!name) ? e.preventDefault() : null}
                     to={`/chatroom?name=${name}&room=${roomItem.roomName}`}
